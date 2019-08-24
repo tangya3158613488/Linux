6d4036a306097129b07e580f76e4c383bdc1ac90
tangya3158613488 <3158613488@qq.com>
2019-08-24 03:20:47 -0700

    add code

---
+++ FileDescriptor/dup2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main()
+{
+    int fd=open("./log",O_CREAT|O_RDWR);
+    if(fd<0)
+    {
+        perror("open");
+        return 1;
+    }
+    close(1);
+    dup2(fd,1);
+    while(1)
+    {
+        char buf[1024]={0};
+        ssize_t read_size=read(0,buf,sizeof(buf)-1);
+        if(read_size<0)
+        {
+            perror("read");
+            break;
+        }
+        printf("%s",buf);
+        fflush(stdout);
+    }
+    return 0;
+}
---
+++ FileDescriptor/test.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+int main()
+{
+    close(1);
+    int fd=open("myfile",O_WRONLY|O_CREAT,00644);
+    if(fd<0)
+    {
+        perror("open");
+        return 1;
+    }
+    printf("fd: %d\n",fd);
+    fflush(stdout);
+
+    close(fd);
+    exit(0);
+}
